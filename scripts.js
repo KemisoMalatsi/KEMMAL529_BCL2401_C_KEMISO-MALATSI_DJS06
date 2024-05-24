52a372f5be1579f8b0bde002a766ca0bae8ba286
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-05-24 13:28:27 +0200

    First group the variables together. Use 'forEach' to log each name on the console. Then use 'forEach' to log both name and its matching province to the console. Inside the product array, I just aligned the price key so that I can easily read the code.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
-// A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+/* 
+ **ForEach Basics**: Use `forEach` to log each name and each province to the console. Then, log each name with a matching province in the format "Name (Province)".
+ */
 
+
+
+
+// A list of provinces:
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']; 
+
+// log each name and province to the console 
+provinces.forEach( (province) => console.log(province));
+names.forEach((name) => console.log(name))
+
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`)
+})
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
+  { product: 'banana',  price: "2" },
+  { product: 'mango',   price: 6 },
+  { product: 'potato',  price: ' ' },
   { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
+  { product: 'coffee',  price: 10 },
+  { product: 'tea',     price: '' },
 ]
